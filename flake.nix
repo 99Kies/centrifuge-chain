e874ec3407e62af12ab0b53345540f9ffe8dd5dd
Lorenzo Manacorda <lorenzo@mailbox.org>
2022-02-28 20:54:21 +0100

    nix: update vendor hash

--- flake.nix
+++ flake.nix
@@ -95,7 +95,7 @@
           };
 
           # This is a hash of all the Cargo dependencies, for reproducibility.
-          cargoSha256 = "sha256-YcmLOVGNBw/v6sCoE3W+bV7mtAm9KsA7dg0pod1rTNk=";
+          cargoSha256 = "sha256-tsndlzK8g+DWl7nL+mEnyP89pYEmf7jVtELYRZnpxk0=";
 
           nativeBuildInputs = with pkgs; [ clang git-mock pkg-config ];
           buildInputs = [ pkgs.openssl ];
